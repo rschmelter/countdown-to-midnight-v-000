0a885daed12290d966b93eefceb7f7c677f0fb74
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-18 22:08:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-# def countdown(number)
-#     number = 10
-#     while number > 0
-#     puts "#{number} SECOND(S)!"
-#     number -+ 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
+def countdown_output(number)
+    number = 10
+    while number > 0
+    puts "#{number} SECOND(S)!"
+    number -+ 1
+  end
+  "HAPPY NEW YEAR!"
+end
